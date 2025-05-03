b17e3fddb2ec135062890ecc88a0d63600f4d0ec
Irfan Ansari <irfangittech@gmail.com>
2025-05-03 19:09:00 +0530

    un doc

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 use App\Http\Controllers\Api\PostApiController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Middleware\ApiMiddleware;
 
 Route::get('/posts', [PostApiController::class, 'index']);
 Route::get('/posts/{slug}', [PostApiController::class, 'show']);
